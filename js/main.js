8ba346c3f55c03d77ba135debe04d89dd7ca5ed1
Tanuv Nair <tanuvnair@gmail.com>
2023-04-05 22:08:39 +0530

    Minor changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('.nav-links');
-console.log('sections length', sections.length);
-console.log('section 0', sections[0]);
-console.log('section 1', sections[1]);
-console.log('section 2', sections[2]);
-console.log('section 3', sections[3]);
-
 
 window.addEventListener('scroll', ()=> {
     let current = '';
