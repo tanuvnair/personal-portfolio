1e16a780c434639da14ac6790f09a7ff218c26ce
Tanuv Nair <tanuvnair@gmail.com>
2023-04-05 21:59:25 +0530

    Fixed navbar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('.nav-links');
+console.log('sections length', sections.length);
+console.log('section 0', sections[0]);
+console.log('section 1', sections[1]);
+console.log('section 2', sections[2]);
+console.log('section 3', sections[3]);
+
 
 window.addEventListener('scroll', ()=> {
     let current = '';
 
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        
+        const sectionHeight = section.clientHeight;        
+
         if(scrollY >= (sectionTop - 200)) {
             current = section.getAttribute('id');
         }
@@ -23,7 +29,7 @@ window.addEventListener('scroll', ()=> {
 
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('header');
-    if (window.pageYOffset > 450) {
+    if (window.pageYOffset > 600) {
       navbar.classList.add('sticky');
     } else {
       navbar.classList.remove('sticky');
