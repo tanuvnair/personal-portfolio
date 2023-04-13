b7b65229410046bb1b0e35c3d55048774ca7d006
Tanuv Nair <tanuvnair@Rajnis-MacBook-Air.local>
2023-04-13 21:18:27 +0530

    Added word count in textarea

--- js/main.js
+++ js/main.js
@@ -1,31 +1,52 @@
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('.nav-links');
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".nav-links");
 
-window.addEventListener('scroll', ()=> {
-    let current = '';
+window.addEventListener("scroll", () => {
+    let current = "";
 
-    sections.forEach(section => {
+    sections.forEach((section) => {
         const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;        
+        const sectionHeight = section.clientHeight;
 
-        if(scrollY >= (sectionTop - 200)) {
-            current = section.getAttribute('id');
+        if (scrollY >= sectionTop - 200) {
+            current = section.getAttribute("id");
         }
-    }); 
+    });
 
-    navLi.forEach(li => {
-        li.classList.remove('active');
-        if(li.classList.contains(current)) {
-            li.classList.add('active');
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+            li.classList.add("active");
         }
-    })
-})
+    });
+});
 
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('header');
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector("header");
     if (window.pageYOffset > 600) {
-      navbar.classList.add('sticky');
+        navbar.classList.add("sticky");
     } else {
-      navbar.classList.remove('sticky');
+        navbar.classList.remove("sticky");
     }
+});
+
+$("#message").keyup(function countWord() {
+    var message = document.getElementById("message").value;
+    message = message.trim();
+
+    var wordCount = 0;
+
+    for (var i = 0; i < message.length; i++) {
+        var currentCharacter = message[i];
+
+        if (currentCharacter == " " && message[i + 1] != " ") {
+            wordCount++;
+        }
+    }
+
+    if (message.length >= 1) {
+        wordCount++;
+    }
+
+    document.getElementById("currentWordCount").innerHTML = wordCount;
 });
